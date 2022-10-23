8a3a3143ca59d8cb94a66713d2cb37a856bb3ebb
freedomwebcn <freedomweb@yeah.net>
2022-10-24 07:22:01 +0800

    测试图片懒加载九

--- src/pages/NewsInfo/index.vue
+++ src/pages/NewsInfo/index.vue
@@ -22,7 +22,10 @@
           <ul>
             <li v-for="userObj in item.content" :key="userObj.imgUrls[0]">
               <!-- <lazy-component> -->
-                  <img v-lazy="imgUrl" v-for="imgUrl in userObj.imgUrls" :key="imgUrl" />
+                <template v-for="imgUrl in userObj.imgUrls" :key="imgUrl">
+                  <img v-lazy="imgUrl"  />
+
+                </template>
               <!-- </lazy-component> -->
             </li>
           </ul>
