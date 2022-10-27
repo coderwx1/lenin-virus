26ab9212a2167b26dd9bb2428f5541421b338472
freedomwebcn <freedomweb@yeah.net>
2022-10-27 09:30:12 +0800

    修改了样式二

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -58,7 +58,7 @@
     <div class="loading" style="width: 100%; height: 100vh" v-else>
       <van-loading type="spinner" vertical>加载中...</van-loading>
     </div>
-    <div class="footer" v-if="newsData.length">
+    <div class="footer" >
       <div class="footer-content">
         <p>
           <span>献给那些</span>
